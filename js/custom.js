7e905fad5b3e6bf0e1a1fc38a8ccdfd92cb1bd9c
Saravanakumar Kathirvel <saravanakumar_kathirvel@epam.com>
2022-08-06 12:07:10 +0530

    final task

--- js/custom.js
+++ js/custom.js
@@ -1,47 +1,164 @@
 (function () {
-  var burger = document.querySelector(".burger");
-  var menu = document.querySelector("#" + burger.dataset.target);
+  let burger = document.querySelector(".burger");
+  let menu = document.querySelector("#" + burger.dataset.target);
   burger.addEventListener("click", function () {
     burger.classList.toggle("is-active");
     menu.classList.toggle("is-active");
   });
-  document.querySelector(".time-dom").innerHTML = "";
-  setInterval(() => {
-    document.querySelector(".time-dom").innerHTML = "";
-    setTimeout(() => {
-      document.querySelector(
-        ".time-dom"
-      ).innerHTML = `Current time is ${new Date().getHours()}:${new Date().getMinutes()}: Minutes ${new Date().getSeconds()}: Seconds`;
-    });
+})();
+
+(function () {
+  setInterval(function () {
+    const date = new Date();
+    document.getElementById(
+      "ariaLive"
+    ).innerHTML = `Thanks for spending 1 more minute in my portal, Time now is ${date.toLocaleTimeString()}`;
   }, 60000);
 })();
 
-document.querySelectorAll("#nav li").forEach(function (navEl) {
-  navEl.onclick = function () {
-    toggleTab(this.id, this.dataset.target);
+(function () {
+  const TABLIST = document.querySelector(".tablist");
+  const TABS = [...TABLIST.querySelectorAll(".tab")];
+  const TABPANELS = [...document.querySelectorAll(".tabpanel")];
+
+  const showActivePanel = (element) => {
+    const selectedId = element.id;
+    TABPANELS.forEach((e) => {
+      e.style.display = "none";
+    });
+    const activePanel = document.querySelector(
+      `[aria-labelledby="#${selectedId}"]`
+    );
+    activePanel.removeAttribute("tabindex", "-1");
+    activePanel.style.display = "block";
   };
-});
 
-function toggleTab(selectedNav, targetId) {
-  var navEls = document.querySelectorAll("#nav li");
-
-  navEls.forEach(function (navEl) {
-    if (navEl.id == selectedNav) {
-      navEl.classList.add("is-active");
-    } else {
-      if (navEl.classList.contains("is-active")) {
-        navEl.classList.remove("is-active");
+  const setSelectedTab = (element) => {
+    const selectedId = element.id;
+    TABS.forEach((e) => {
+      const id = e.getAttribute("id");
+      if (id === selectedId) {
+        e.removeAttribute("tabindex", "0");
+        e.setAttribute("aria-selected", "true");
+        e.focus();
+        e.classList.add("is-active");
+      } else {
+        e.setAttribute("tabindex", "-1");
+        e.setAttribute("aria-selected", "false");
+        e.classList.remove("is-active");
       }
+    });
+  };
+
+  const createArrowNavigation = () => {
+    const firstTab = TABS[0];
+    const lastTab = TABS[TABS.length - 1];
+
+    TABS.forEach((element) => {
+      element.addEventListener("keydown", function (e) {
+        if ((e.keyCode || e.which) === 38 || (e.keyCode || e.which) === 37) {
+          if (element == firstTab) {
+            e.preventDefault();
+            lastTab.focus();
+          } else {
+            e.preventDefault();
+            const focusableElement = TABS.indexOf(element) - 1;
+            TABS[focusableElement].focus();
+          }
+        } else if (
+          (e.keyCode || e.which) === 40 ||
+          (e.keyCode || e.which) === 39
+        ) {
+          if (element == lastTab) {
+            e.preventDefault();
+            firstTab.focus();
+          } else {
+            e.preventDefault();
+            const focusableElement = TABS.indexOf(element) + 1;
+            TABS[focusableElement].focus();
+          }
+        }
+      });
+    });
+  };
+
+  const handleClick = () => {
+    TABS.forEach((element) => {
+      element.addEventListener("click", function () {
+        setSelectedTab(element);
+        showActivePanel(element);
+      });
+    });
+  };
+  handleClick();
+  createArrowNavigation();
+})();
+
+(function () {
+  const menuItems = document.querySelectorAll(".navbar-item.has-submenu");
+  menuItems.forEach((item) => {
+    if (item.children) {
+      item.addEventListener("click", menuItemHandler);
     }
   });
+})();
 
-  var tabs = document.querySelectorAll(".tab-pane");
+function menuItemHandler(e) {
+  const current = e.currentTarget;
+  if (current?.children[0]?.getAttribute("aria-expanded") === "false") {
+    current.querySelector(".fas").classList.remove("fa-angle-down");
+    current.querySelector(".fas").classList.add("fa-angle-up");
+    current.children[1].style.display = "block";
+    (current.children[1]?.children[0]).focus();
+    current.children[0].setAttribute("aria-expanded", true);
+    if (current.children[0].classList.contains("skip"))
+      current?.children[0].classList.remove("visually-hidden");
+  } else {
+    current.querySelector(".fas").classList.remove("fa-angle-up");
+    current.querySelector(".fas").classList.add("fa-angle-down");
+    current.children[1].style.display = "none";
+    current.children[0].setAttribute("aria-expanded", false);
+  }
+  document.querySelectorAll(".sub-menu").forEach((item) => {
+    const validChildren = Object.values(item.children).filter(
+      (ch) => ch.role !== "separator"
+    );
 
-  tabs.forEach(function (tab) {
-    if (tab.id == targetId) {
-      tab.style.display = "block";
-    } else {
-      tab.style.display = "none";
-    }
+    let menuItemFocus = 0;
+
+    item.addEventListener("keydown", (e) => {
+      e.preventDefault();
+      validChildren[menuItemFocus].setAttribute("tabindex", 0);
+      if (e.keyCode === 40) {
+        menuItemFocus++;
+        if (menuItemFocus >= validChildren.length) {
+          //down arrow
+          menuItemFocus = 0;
+        }
+        validChildren[menuItemFocus].setAttribute("tabindex", -1);
+        validChildren[menuItemFocus].focus();
+      } else if (e.keyCode === 38) {
+        menuItemFocus--;
+        if (menuItemFocus < 0) {
+          // up arrow
+          menuItemFocus = validChildren.length - 1;
+        }
+
+        validChildren[menuItemFocus].setAttribute("tabindex", -1);
+        validChildren[menuItemFocus].focus();
+      } else if (e.keyCode === 13 || e.keyCode === 32) {
+        e.preventDefault();
+        validChildren[menuItemFocus].firstElementChild.click(); // enter and space to enter submenu
+        validChildren[menuItemFocus].focus();
+        if (current.children[0].classList.contains("skip"))
+          current?.children[0].classList.add("visually-hidden");
+      } else if (e.keyCode === 27) {
+        e.preventDefault();
+        current.children[1].style.display = "none"; // escape
+        current.children[0].setAttribute("aria-expanded", false);
+        if (current.children[0].classList.contains("skip"))
+          current?.children[0].classList.add("visually-hidden");
+      }
+    });
   });
 }
